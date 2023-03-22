c0a7da07b31c13776e4c24a5ef34eeefff8fa818
vivek9211 <82077595+vivek9211@users.noreply.github.com>
2023-03-22 23:36:41 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  topHeadlinesURL: 'https://newsapi.org/v2/top-headlines?',
+  sourcesURL: 'https://newsapi.org/v2/sources?',
+  everythingURL: 'https://newsapi.org/v2/everything?',
+  countryID: 'country=id&',
+  apiKey: 'apiKey=aed4690b824e429f899ce501ece27b7a',
+  sources: '&sources=',
+  search: '&q='
+}
